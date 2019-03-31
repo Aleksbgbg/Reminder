c1fe6aea68f1457be0370ff343dfcd1b61376d06
Aleksbgbg <kozichka01@abv.bg>
2019-03-31 10:33:59 +0100

    [ReminderTests] Add tests for Reminder End date.

---
+++ Reminder.Tests/Models/ReminderTests.cs
@@ -0,0 +1,25 @@
+﻿namespace Reminder.Tests.Models
+{
+    using System;
+
+    using Reminder.Models;
+
+    using Xunit;
+
+    public class ReminderTests
+    {
+        [Fact]
+        public void TestEndsAtCorrectTime()
+        {
+            // Arrange
+            DateTime start = new DateTime(2020, 6, 15);
+            TimeSpan duration = TimeSpan.FromDays(3);
+
+            // Act
+            Reminder reminder = new Reminder(string.Empty, start, duration);
+
+            // Assert
+            Assert.Equal(start + duration, reminder.End);
+        }
+    }
+}
